e6286d1f5d3a9d9e00840cc7e5b194ec95b70c21
Carly Lucas-Melanson <carlylm@indeed.com>
2020-02-23 20:44:47 -0600

    NOBUG finish implementation 3/6.

--- src/main/java/CoffeeMachine.java
+++ src/main/java/CoffeeMachine.java
@@ -1,21 +1,42 @@
 package main.java;
 
 import java.util.Scanner;
+import java.lang.Math;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("How many cups of coffee do you need?");
-        int numCupsOfCoffee = scanner.nextInt();
-        int mlWater = numCupsOfCoffee * 200;
-        int mlMilk = numCupsOfCoffee * 50;
-        int gramsOfBeans = numCupsOfCoffee * 15;
+        System.out.println("Water inventory, in ml: ");
+        int mlWaterInMachine = scanner.nextInt();
+        int cupsPossibleWithWaterAmount = mlWaterInMachine / 200;
+        System.out.println("Milk inventory, in ml: ");
+        int mlMilkInMachine = scanner.nextInt();
+        int cupsPossibleWithMilkAmount = mlMilkInMachine / 50;
+        System.out.println("Coffee beans inventory, in g: ");
+        int gBeansInMachine = scanner.nextInt();
+        int cupsPossibleWithBeansAmount = gBeansInMachine / 15;
 
-        System.out.println("For " + numCupsOfCoffee + " cups of coffee you will need:");
-        System.out.println(mlWater + " ml of water.");
-        System.out.println(mlMilk + " ml of milk.");
-        System.out.println(gramsOfBeans + " g of coffee beans.");
+        int minAmountOfCupsPossible = Math.min(Math.min(cupsPossibleWithWaterAmount, cupsPossibleWithMilkAmount), cupsPossibleWithBeansAmount);
+
+        System.out.println("How many cups of coffee do you need? ");
+        int numCupsOfCoffeeDesired = scanner.nextInt();
+
+        int mlWaterNeeded = numCupsOfCoffeeDesired * 200;
+        int mlMilkNeeded = numCupsOfCoffeeDesired * 50;
+        int gramsOfBeansNeeded = numCupsOfCoffeeDesired * 15;
+
+        boolean ableToMakeCoffee = mlWaterInMachine >= mlWaterNeeded &&
+                mlMilkInMachine >= mlMilkNeeded &&
+                gBeansInMachine >= gramsOfBeansNeeded;
+
+        if (ableToMakeCoffee) {
+            System.out.println("Yes, I am able to make " + numCupsOfCoffeeDesired + " cups of coffee.");
+            System.out.println("I can even make " + (minAmountOfCupsPossible - numCupsOfCoffeeDesired) + " more than that!");
+        } else {
+            System.out.println("No, I am unable to make that many cups of coffee.");
+            System.out.println("I am only able to make " + minAmountOfCupsPossible + " cups of coffee.");
+        }
     }
 
 }
